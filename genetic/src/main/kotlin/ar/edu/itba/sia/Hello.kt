372c09ab9db46f5a54e68bbd65ac0564f6f8f8a7
Julian Nicastro <julrodriguez@itba.edu.ar>
2018-05-16 15:31:06 -0300

    Genetics set up

---
+++ genetic/src/main/kotlin/ar/edu/itba/sia/Hello.kt
@@ -0,0 +1,6 @@
+package ar.edu.itba.sia
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ genetic/src/test/kotlin/ar/edu/itba/sia/HelloTest.kt
@@ -0,0 +1,8 @@
+package ar.edu.itba.sia
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
