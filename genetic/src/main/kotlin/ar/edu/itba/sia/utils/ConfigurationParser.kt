d53b0fa2e268538ccfb5d5a072df22e6c12f437c
Juan Franco <juan.caracciolo@wolox.com.ar>
2018-05-21 09:20:39 -0300

    Add configuration file

--- genetic/src/main/kotlin/ar/edu/itba/sia/utils/ConfigurationParser.kt
+++ genetic/src/main/kotlin/ar/edu/itba/sia/utils/ConfigurationParser.kt
@@ -6,9 +6,12 @@ import ar.edu.itba.sia.Engine.mutators.Mutator
 import ar.edu.itba.sia.Engine.mutators.NotUniformMutator
 import ar.edu.itba.sia.Engine.mutators.UniformMutator
 import ar.edu.itba.sia.Engine.replacer.*
+import ar.edu.itba.sia.Engine.selector.EliteSelector
 import ar.edu.itba.sia.Engine.selector.Selector
 import org.json.JSONException
 import org.json.JSONObject
+import java.io.File
+import java.nio.charset.Charset
 import kotlin.system.exitProcess
 
 enum class Crossers(val string: String, val crosser: Crosser) {
@@ -17,8 +20,10 @@ enum class Crossers(val string: String, val crosser: Crosser) {
     DOUBLE_POINT("double point", DoublePointCrosser()),
     UNIFORM("uniform", UniformCrosser());
 
-    fun fromSting(string: String): Crosser = Crossers.values().firstOrNull { it.string == string }
-            .let { it ?: println("$string is not a valid crosser");exitProcess(-1) }
+    companion object {
+        fun fromSting(string: String): Crosser = Crossers.values().firstOrNull { it.string == string }
+                .let { it?.crosser ?: "$string is not a valid crosser".andExit() }
+    }
 }
 
 enum class Cutters(val string: String) {
@@ -27,32 +32,31 @@ enum class Cutters(val string: String) {
     NO_PROGRESS("no progress"),
     OPTIMAL_CUTTER("optimal");
 
-    fun fromSting(string: String, parameters: JSONObject): Cutter =
-        when(string) {
-            GENERATION.string -> GenerationCutter(
-                    parameters.tryWithError("generations", Int::class.java)
-            )
+    companion object {
+        fun fromString(string: String, parameters: JSONObject): Cutter =
+                when(string) {
+                    GENERATION.string -> GenerationCutter(
+                            parameters.tryWithError("generations", Int::class.java)
+                    )
 
-            STAYS_SAME.string -> GenerationStaysSame(
-                    parameters.tryWithError("generations", Int::class.java),
-                    parameters.tryWithError("amount", Int::class.java)
-            )
+                    STAYS_SAME.string -> GenerationStaysSame(
+                            parameters.tryWithError("generations", Int::class.java),
+                            parameters.tryWithError("amount", Int::class.java)
+                    )
 
-            NO_PROGRESS.string -> NoProgressCutter(
-                    parameters.tryWithError("generations", Int::class.java),
-                    parameters.tryWithError("percentage", Double::class.java)
-            )
+                    NO_PROGRESS.string -> NoProgressCutter(
+                            parameters.tryWithError("generations", Int::class.java),
+                            parameters.tryWithError("percentage", Double::class.java)
+                    )
 
-            OPTIMAL_CUTTER.string -> OptimalCutter(
-                    parameters.tryWithError("optimal", Double::class.java)
-            )
+                    OPTIMAL_CUTTER.string -> OptimalCutter(
+                            parameters.tryWithError("amount", Double::class.java)
+                    )
 
-            else -> {
-                println("$string is not a valid Cutter")
-                exitProcess(-1)
-            }
+                    else -> "$string is not a valid Cutter".andExit()
 
-        }
+                }
+    }
 
     }
 
@@ -60,8 +64,10 @@ enum class Mutators(val string: String, val mutator: Mutator) {
     NOT_UNIFORM("not uniform", NotUniformMutator()),
     UNIFORM("uniform", UniformMutator());
 
-    fun fromSting(string: String): Mutator = Mutators.values().firstOrNull { it.string == string }
-            .let { it ?: println("$string is not a valid mutator"); exitProcess(-1) }
+    companion object {
+        fun fromSting(string: String): Mutator = Mutators.values().firstOrNull { it.string == string }
+                .let { it?.mutator ?: "$string is not a valid mutator".andExit() }
+    }
 }
 
 enum class Replacers(val string: String) {
@@ -70,56 +76,46 @@ enum class Replacers(val string: String) {
     MIX_MATCH_REPLACER("mix and match"),
     COMBINED("combined");
 
-    fun fromSting(string: String, selector: Selector, parameters: JSONObject?): Replacer =
-            when(string) {
-                LESS_CHILDREN.string -> {
-                    if(parameters==null) { println("Less children replacer needs amount parameter"); exitProcess(-1) }
-                    LessChildrenReplacer(
-                            parameters.tryWithError("amount", Int::class.java),
-                            selector
-                    )
+    companion object {
+        fun fromSting(string: String, selector: Selector, parameters: JSONObject?): Replacer =
+                when (string) {
+                    LESS_CHILDREN.string -> {
+                        LessChildrenReplacer(
+                                parameters?.tryWithNull("amount", Int::class.java),
+                                selector
+                        )
+                    }
+
+                    ONLY_CHILDREN.string -> OnlyChildrenReplacer(selector)
+
+                    MIX_MATCH_REPLACER.string -> {
+                        MixAndMatchReplacer(
+                                parameters?.tryWithNull("amount", Int::class.java),
+                                selector
+                        )
+                    }
+
+                    COMBINED.string -> {
+                        if (parameters == null) {
+                            "Combined replacer needs parameters".andExit()
+                        }
+                        CombinedReplacer(
+                                parameters.tryWithError("percentage", Double::class.java),
+                                parameters.tryWithError("first",JSONObject::class.java).toReplacer(selector),
+                                parameters.tryWithError("second", JSONObject::class.java).toReplacer(selector)
+                        )
+                    }
+
+                    else -> "$string is not a valid Replacer".andExit()
                 }
-
-                ONLY_CHILDREN.string -> OnlyChildrenReplacer(selector)
-
-                MIX_MATCH_REPLACER.string -> {
-                    if(parameters==null) { println("Mix and Match replacer needs amount parameter"); exitProcess(-1) }
-                    MixAndMatchReplacer(
-                            parameters.tryWithError("amount", Int::class.java),
-                            selector
-                    )
-                }
-
-                COMBINED.string -> {
-                    if(parameters==null) { println("Combined replacer needs parameters"); exitProcess(-1) }
-                    CombinedReplacer(
-                            parameters.tryWithError("percentage", Double::class.java),
-
-                            fromSting(
-                                    parameters.tryWithError("first", String::class.java),
-                                    selector,
-                                    parameters.tryWithError("parameters", JSONObject::class.java)
-                            ),
-
-                            fromSting(
-                                    parameters.tryWithError("first", String::class.java),
-                                    selector,
-                                    parameters.tryWithError("parameters", JSONObject::class.java)
-                            )
-                    )
-                }
-
-                else -> {
-                    println("$string is not a valid Replacer")
-                    exitProcess(-1)
-                }
-
-            }
-
+    }
 }
 
 data class ConfigurationFile(
-    val crosser: Crossers
+    val crosser: Crossers,
+    val cutter: Cutter,
+    val mutator: Mutator,
+    val replacer: Replacer
 )
 
 class ConfigurationParser {
@@ -127,15 +123,21 @@ class ConfigurationParser {
 
         @JvmStatic
         fun main(args: Array<String>) {
-            val jsonString = "{ Crosser:\"AAA\", king: 1 }"
+            val jsonString = File("./src/Resources/config.json").readText(Charset.defaultCharset())
             val jsonObject = JSONObject(jsonString)
 //            ConfigurationFile(jsonObject.tryWithError("crosser"))
 
-            print(jsonObject.tryWithError("king", String::class.java))
+            println("AAA")
+
+            val crosser = jsonObject.tryWithError("crosser", String::class.java).toCrosser()
+            val cutter = jsonObject.tryWithError("cutter", JSONObject::class.java).toCutter()
+            val mutator = jsonObject.tryWithError("mutator", String::class.java).toMutator()
+            val replacer = jsonObject.tryWithError("replacer", JSONObject::class.java).toReplacer(EliteSelector())
+            println("DONE")
+
         }
 
         fun parseFile(path: String): ConfigurationFile? {
-//            val jsonString = File(path).readText(Charset.defaultCharset())
 
 
 
@@ -148,17 +150,38 @@ class ConfigurationParser {
 }
 
 inline fun <reified T> JSONObject.tryWithError(key: String, clazz: Class<T>): T =
-    try {
-        try {
-            val value = this[key]
-            value as T
-        } catch (e: ClassCastException) {
-            println("[ERROR] Expected value for key ($key) to be of type ${T::class.java.simpleName} but was of type ${this[key]::class.java.simpleName} ")
-            exitProcess(-1)
-        }
-    } catch (e: JSONException) {
-        println("[ERROR] Key ($key) not found on the JSON Object")
-        exitProcess(-1)
-    }
+    this.tryWithNull(key, clazz) ?:  "[ERROR] Key ($key) not found on the JSON Object".andExit()
 
-fun String.toCrossers(): Crossers = Crossers.valueOf(this)
+inline fun <reified T> JSONObject.tryWithNull(key: String, clazz: Class<T>): T? =
+        try {
+            try {
+                val value = this[key]
+                when(value) {
+                    JSONObject.NULL -> null
+                    else -> value as T
+                }
+            } catch (e: ClassCastException) {
+                "[ERROR] Expected value for key ($key) to be of type ${T::class.java.simpleName} but was of type ${this[key]::class.java.simpleName}".andExit()
+            }
+        } catch (e: JSONException) {
+            null
+        }
+
+fun String.toCrosser(): Crosser = Crossers.fromSting(this)
+fun String.toMutator(): Mutator = Mutators.fromSting(this)
+
+fun JSONObject.toCutter(): Cutter = Cutters.fromString(
+        this.tryWithError("type", String::class.java),
+        this.tryWithError("parameters", JSONObject::class.java)
+        )
+
+fun JSONObject.toReplacer(selector: Selector): Replacer = Replacers.fromSting(
+        this.tryWithError("type", String::class.java),
+        selector,
+        this.tryWithNull("parameters", JSONObject::class.java)
+)
+
+fun String.andExit(): Nothing {
+    println(this)
+    exitProcess(-1)
+}
