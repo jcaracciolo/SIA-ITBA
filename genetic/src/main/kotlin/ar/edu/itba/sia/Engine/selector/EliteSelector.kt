7886362bff4b45a2c01a55b625e8beb7a0d4bd73
Sebastian Guido <seguido@Sebastians-MacBook-Pro.local>
2018-05-21 22:33:54 -0300

    selector fix

--- genetic/src/main/kotlin/ar/edu/itba/sia/Engine/selector/EliteSelector.kt
+++ genetic/src/main/kotlin/ar/edu/itba/sia/Engine/selector/EliteSelector.kt
@@ -8,12 +8,7 @@ class EliteSelector: Selector {
     override fun <G> select(generation: List<Evolutionable<G>>, amount: Int): List<Evolutionable<G>> {
         var selectedSpecimens = ArrayList<Evolutionable<G>>()
 
-        Comparator
-        generation.sortedWith(Comparator(function = (first: Evolutionable<G>, second: Evolutionable<G> -> Int {
-            when{
 
-            }
-        }))
 
         for (index in 0 until amount) {
             selectedSpecimens.add(generation[index])
